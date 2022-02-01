01f0038f2b3b0f8308e8a2fb87486ed68215f220
robdavis <robertldavis@cox.net>
2022-02-01 13:51:20 -0700

    Final Comments, need to bugtest

--- src/Game.java
+++ src/Game.java
@@ -4,13 +4,20 @@ public class Game {
 
 	public static void main(String[] args) {
 		Scanner scan = new Scanner(System.in);
+		
 		System.out.println("Welcome to Yahtzee!");
+		
 		System.out.println("How many players will there be?");
 		int total = scan.nextInt();
+		
 		Player[] players = new Player[total];
+		
+		// initialize player objects in array
 		for (int i = 0; i < players.length; i++) {
 			players[i] = new Player();
 		}
+		
+		// take 13 turns, cycling through all players for each turn
 		for (int turn = 1; turn <= 13; turn++) {
 			for (int i = 0; i < players.length; i++) {
 				players[i].StartTurn();
--- src/Player.java
+++ src/Player.java
@@ -7,6 +7,9 @@ public class Player {
 	private ScoreSheet scoring;
 	private int playerID;
 	
+	/**
+	 * Player constructor makes a new dicecup, scoresheet, and playerID, while increasing the total players int
+	 */
 	public Player() {
 		dice = new DiceCup();
 		scoring = new ScoreSheet();
@@ -14,26 +17,37 @@ public class Player {
 		playerID = numPlayers;
 	}
 	
+	/**
+	 * This contains the turn logic for each player's turn
+	 */
 	public void StartTurn() {
+		
 		Scanner scan = new Scanner(System.in);
 		System.out.println("It is now Player " + playerID + "'s Turn.");
+		
 		for (int i = 0; i < 3; i++) {
-			System.out.println("Roll " + i + ": ");
+			System.out.println("Roll " + (i + 1) + ": ");
+			
 			dice.RollDice();
+			
 			dice.PrintDice();
 			System.out.println("Which dice do you want to keep?");
 			System.out.println("Enter 1-5 to Toggle the Die or -1 to Roll");
+			
 			int input = 0;
 			while (input != -1) {
 				dice.PrintWithKeep();
 				input = scan.nextInt();
 				dice.Keep(input);
 			}
+			
 		}
 		System.out.println("Scoring Turn For Player " + playerID);
 		dice.PrintDice();
+		
 		scoring.setDiceCup(dice);
 		scoring.showAndSet();
+		
 		dice.CompleteReroll();
 	}
 	
